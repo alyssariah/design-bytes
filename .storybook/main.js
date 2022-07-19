1ee6e4ae258b3d2515b041da4d2e48d48782d5fc
Alyssa Jackson <alyssariah@alyssas-mbp.lan>
2022-07-19 01:04:04 -0400

    incorporate colors and reorganize file structure

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,13 @@
 module.exports = {
   stories: [
-    '../src/stories/*.stories.mdx',
-    '../src/stories/*.stories.@(js|jsx|ts|tsx)',
-    '../src/stories/**/*.stories.mdx',
-    '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/stories/**/**/*.stories.mdx',
-    '../src/stories/**/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/stories/**/**/**/*.stories.mdx',
-    '../src/stories/**/**/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/**/*.stories.mdx',
+    '../src/**/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/**/**/*.stories.mdx',
+    '../src/**/**/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/**/**/**/*.stories.mdx',
+    '../src/**/**/**/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
