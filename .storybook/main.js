91313138d5a75671fb1d214d4052eb79e6da0771
alyssariah <alyssariah@gmail.com>
2022-09-11 14:21:33 -0400

    fix storybook errors

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,20 +16,15 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        cssLoaderOptions: {
-          // When you have splitted your css over multiple files
-          // and use @import('./other-styles.css')
-          importLoaders: 1,
-        },
-        postcssLoaderOptions: {
-          // When using postCSS 8
-          implementation: require('postcss'),
-        },
-      },
-    },
+    '@storybook/preset-scss',
+    // {
+    //   name: '@storybook/addon-postcss',
+    //   options: {
+    //     postcssLoaderOptions: {
+    //       implementation: require('postcss'),
+    //     },
+    //   },
+    // },
   ],
   framework: '@storybook/react',
   core: {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import '/src/index.scss';
+import 'tailwindcss/tailwind.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
